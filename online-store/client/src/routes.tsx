9d3e0d1d917a7485d5c6567fa8e03f8a209a96c5
Karken Turarov <karken_turarov@mail.ru>
2024-02-08 18:37:46 +0600

    edit routes

--- online-store/client/src/routes.tsx
+++ online-store/client/src/routes.tsx
@@ -1,18 +1,44 @@
 import React from "react";
 import Admin from "./pages/Admin";
 import Basket from "./pages/Basket";
+import {
+  ADMIN_ROUTE,
+  BASKET_ROUTE,
+  DEVICE_ROUTE,
+  LOGIN_ROUTE,
+  REGISTRATION_ROUTE,
+  SHOP_ROUTE,
+} from "./utils/const";
+import Shop from "./pages/Shop";
+import Device from "./pages/DevicePage";
+import Auth from "./pages/Auth";
 
-export const authRoutes = () => [
+export const authRoutes = [
   {
-    path: "/admin",
+    path: ADMIN_ROUTE,
     Component: Admin,
   },
   {
-    path: "/basket",
+    path: BASKET_ROUTE,
     Component: Basket,
   },
 ];
 
-export const publicRoutes = () => {
-  return <div></div>;
-};
+export const publicRoutes = [
+  {
+    path: SHOP_ROUTE,
+    Component: Shop,
+  },
+  {
+    path: LOGIN_ROUTE,
+    Component: Auth,
+  },
+  {
+    path: REGISTRATION_ROUTE,
+    Component: Auth,
+  },
+  {
+    path: DEVICE_ROUTE + "/:id",
+    Component: Device,
+  },
+];
